a367a7497e278fd95895afd7c0841ab4ea1cda1e
Leon_2802 <leon.gobbert@hs-furtwangen.de>
2021-01-23 22:49:33 +0100

    card shuffle backside

--- memory.js
+++ memory.js
@@ -1,22 +1,64 @@
 //Schlüssel für EIA-Satz!
 //Var-Elemente sammeln:
-var stufe1 = document.getElementById("eins");
-var stufe2 = document.getElementById("zwei");
-var stufe3 = document.getElementById("drei");
+var stufe1 = document.querySelector("#einfach");
+var stufe2 = document.querySelector("#normal");
+var stufe3 = document.querySelector("#schwer");
+var easyContainer = document.querySelector("#easy-cards");
+var mediumContainer = document.querySelector("#medium-cards");
+var hardContainer = document.querySelector("#hard-cards");
+//????? Warum lädt der window.eventlistener meine deklarationen nicht?!
 //Booleans für die Schwierigkeiten:
 var easy = false;
 var medium = false;
 var hard = false;
+//weitere Bools:
+var lockDifficulty = false;
 //Event-Listener für die verschiedenen Schalter:
 stufe1.addEventListener("click", function () {
     easy = true;
+    createCarddeck();
 });
 stufe2.addEventListener("click", function () {
     medium = true;
+    createCarddeck();
 });
 stufe3.addEventListener("click", function () {
     hard = true;
+    createCarddeck();
 });
 function createCarddeck() {
+    if (easy == true && lockDifficulty == false) {
+        easyContainer.classList.remove("isHidden");
+        lockDifficulty = true;
+        for (var indexEasy = 1; indexEasy <= 8; indexEasy++) {
+            pushCardsToDom();
+        }
+    }
+    else if (medium == true && lockDifficulty == false) {
+        mediumContainer.classList.remove("isHidden");
+        lockDifficulty = true;
+        for (var indexMedium = 1; indexMedium <= 16; indexMedium++) {
+            pushCardsToDom();
+        }
+    }
+    else if (hard == true && lockDifficulty == false) {
+        hardContainer.classList.remove("isHidden");
+        lockDifficulty = true;
+        for (var indexHard = 1; indexHard <= 32; indexHard++) {
+            pushCardsToDom();
+        }
+    }
+}
+function pushCardsToDom() {
+    var source = "images/cards_background.png";
+    if (easy == true) {
+        easyContainer.innerHTML += "<img src=" + source + " />";
+    }
+    else if (medium == true) {
+        mediumContainer.innerHTML += "<img src=" + source + " />";
+    }
+    else if (hard == true) {
+        hardContainer.innerHTML += "<img src=" + source + " />";
+    }
 }
 //# sourceMappingURL=memory.js.map
--- memory.ts
+++ memory.ts
@@ -1,13 +1,20 @@
 //Schlüssel für EIA-Satz!
 //Var-Elemente sammeln:
-const stufe1: HTMLElement = document.getElementById("eins");
-const stufe2: HTMLElement = document.getElementById("zwei");
-const stufe3: HTMLElement = document.getElementById("drei");
+var stufe1: HTMLInputElement = document.querySelector("#einfach");
+var stufe2: HTMLElement = document.querySelector("#normal");
+var stufe3: HTMLElement = document.querySelector("#schwer");
+var easyContainer: HTMLElement = document.querySelector("#easy-cards");
+var mediumContainer: HTMLElement = document.querySelector("#medium-cards");
+var hardContainer: HTMLElement = document.querySelector("#hard-cards");
+
+//????? Warum lädt der window.eventlistener meine deklarationen nicht?!
 
 //Booleans für die Schwierigkeiten:
 var easy: boolean = false;
 var medium: boolean = false;
 var hard: boolean = false;
+//weitere Bools:
+var lockDifficulty: boolean = false;
 
 //Interfaces für die Kartendecks:
 interface CardBlueprint {
@@ -18,14 +25,50 @@ interface CardBlueprint {
 //Event-Listener für die verschiedenen Schalter:
 stufe1.addEventListener("click", function(): void {
     easy = true;
+    createCarddeck();
 });
 stufe2.addEventListener("click", function(): void {
     medium = true;
+    createCarddeck();
 });
 stufe3.addEventListener("click", function(): void {
     hard = true;
+    createCarddeck();
 });
 
 function createCarddeck(): void {
+    if (easy == true && lockDifficulty == false) {
+        easyContainer.classList.remove("isHidden");
+        lockDifficulty = true;
+        for (var indexEasy: number = 1; indexEasy <= 8; indexEasy++) {
+            pushCardsToDom();
+        }
+    }
+    else if (medium == true && lockDifficulty == false) {
+        mediumContainer.classList.remove("isHidden");
+        lockDifficulty = true;
+        for (var indexMedium: number = 1; indexMedium <= 16; indexMedium++) {
+            pushCardsToDom();
+        }
+    }
+    else if (hard == true && lockDifficulty == false) {
+        hardContainer.classList.remove("isHidden");
+        lockDifficulty = true;
+        for (var indexHard: number = 1; indexHard <= 32; indexHard++) {
+            pushCardsToDom();
+        }
+    }
+}
 
+function pushCardsToDom(): void {
+    var source: string = "images/cards_background.png";
+    if (easy == true) {
+        easyContainer.innerHTML += "<img src=" + source + " />";
+    }
+    else if (medium == true) {
+        mediumContainer.innerHTML += "<img src=" + source + " />";
+    }
+    else if (hard == true) {
+        hardContainer.innerHTML += "<img src=" + source + " />";
+    }
 }
